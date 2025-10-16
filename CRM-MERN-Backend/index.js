d4ca3d69fd2bea13d4ab5a6d1ece3717f2e260ba
sk tazim <tazim.zenexistech@gmail.com>
2025-10-16 17:20:40 +0530

    cors added

--- CRM-MERN-Backend/index.js
+++ CRM-MERN-Backend/index.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "https://torbit.netlify.app/"
+}))
 app.use(express.json())
 app.use('/',AppRoutes)
 
